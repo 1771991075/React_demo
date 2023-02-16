1694d9bf7d53bea10f892d17f74a250228be6553
lzc <lvzicong123@outlook.com>
2023-02-16 21:26:37 +0800

    路由懒加载

---
+++ src_路由懒加载/views/Order/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Order() {
+  return (
+    <div>
+        <div className='topNav'>
+          <div></div>
+          <div><h2>我的订单</h2></div>
+          <div></div>
+        </div>
+    </div>
+  )
+}
