4db0e33f287d526068e5931953fed2fc64b504f9
lzc <lvzicong123@outlook.com>
2023-02-20 08:38:42 +0800

    config

---
+++ src/views/Mine/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Mine() {
+  return (
+    <div>
+      <div className='topNav'>
+        <div></div>
+        <div><h2>个人中心</h2></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
