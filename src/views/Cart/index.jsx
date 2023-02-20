44035ae6fdb4be2efd6dcbd93303a63177ea2b03
lzc <lvzicong123@outlook.com>
2023-02-20 08:38:55 +0800

    config

---
+++ src/views/Cart/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Cart() {
+  return (
+    <div className='topNav'>
+        <div></div>
+        <div><h2>购物车</h2></div>
+        <div></div>
+      </div>
+  )
+}
